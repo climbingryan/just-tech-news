8504d41bc93fac66799df9b79d1bf4fc168ededb
shaggy <simmons5family@gmail.com>
2021-01-06 23:26:41 -0600

    update get routes to include vote data => user-routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Vote } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -20,6 +20,18 @@ router.get('/:id', (req, res) => {
     User.findOne({
             // ^ SELECT * FROM users WHERE id = x;
         attributes: { exclude: ['password'] },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: 'voted_posts'
+            }
+        ],  
         where: {
             id: req.params.id 
         }
