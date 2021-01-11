a10bef46e2acc9f0c97e3cef4a60bf6388609a5e
shaggy <simmons5family@gmail.com>
2021-01-11 15:52:37 -0600

    users can now login, logout, and create an account

--- server.js
+++ server.js
@@ -6,8 +6,23 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
